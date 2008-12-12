1bbe76ec71a15e526d3f5e8c71a17db3adf4dabc
icbuilders <icbuilders@05b3e5af-d696-470f-a577-fd7599f74d3c>
2008-12-12 21:01:01 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-framework-193.
    git-svn-id: https://svn.forgerock.org/openicf/tags/last-successful@1795 05b3e5af-d696-470f-a577-fd7599f74d3c

---
+++ framework/test-contract/src/org/identityconnectors/contract/data/RandomGeneratorTest.java
@@ -0,0 +1,82 @@
+/*
+ * ====================
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 2007-2008 Sun Microsystems, Inc. All rights reserved.     
+ * 
+ * The contents of this file are subject to the terms of the Common Development 
+ * and Distribution License("CDDL") (the "License").  You may not use this file 
+ * except in compliance with the License.
+ * 
+ * You can obtain a copy of the License at 
+ * http://IdentityConnectors.dev.java.net/legal/license.txt
+ * See the License for the specific language governing permissions and limitations 
+ * under the License. 
+ * 
+ * When distributing the Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at identityconnectors/legal/license.txt.
+ * If applicable, add the following below this CDDL Header, with the fields 
+ * enclosed by brackets [] replaced by your own identifying information: 
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * ====================
+ */
+package org.identityconnectors.contract.data;
+
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * JUnit test class for RandomGenerator
+ * 
+ * @author David Adam
+ * 
+ */
+public class RandomGeneratorTest {
+    private static RandomGenerator rg;
+
+    @Before
+    public void setUp() {
+        rg = new RandomGenerator();
+    }
+
+    @After
+    public void tearDown() {
+        rg = null;
+    }
+
+    @Test
+    public void testRandomLongGenerator() {
+        {
+            Object o = rg.generate("#####", Long.class);
+            Assert.assertNotNull(o);
+            Assert.assertTrue(o instanceof Long);
+            System.out.println(o.toString());
+        }
+
+    }
+
+    @Test
+    public void testRgen2() {
+        {
+            Object o = rg.generate("###X##");
+            Assert.assertNotNull(o);
+            Assert.assertTrue(o.toString().contains("X"));
+            System.out.println(o.toString());
+        }
+    }
+
+    @Test
+    public void testRgen3() {
+        {
+            Object o = rg.generate("###\\.##", Float.class); // this means
+                                                                // ###\.##
+            Assert.assertNotNull(o);
+            Assert.assertTrue(o instanceof Float);
+            Assert.assertTrue(o.toString().contains("."));
+            System.out.println(o.toString());
+        }
+    }
+}
