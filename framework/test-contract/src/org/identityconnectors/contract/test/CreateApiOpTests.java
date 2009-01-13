0f1f146c96ec55aca6c3fbe7cfe4aafd2e397c1b
icbuilders <icbuilders@05b3e5af-d696-470f-a577-fd7599f74d3c>
2009-01-13 09:26:29 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-framework-211.
    git-svn-id: https://svn.forgerock.org/openicf/tags/last-successful@2294 05b3e5af-d696-470f-a577-fd7599f74d3c

---
+++ framework/test-contract/src/org/identityconnectors/contract/test/CreateApiOpTests.java
@@ -0,0 +1,200 @@
+/*
+ * ====================
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 2007-2008 Sun Microsystems, Inc. All rights reserved.     
+ * 
+ * The contents of this file are subject to the terms of the Common Development 
+ * and Distribution License("CDDL") (the "License").  You may not use this file 
+ * except in compliance with the License.
+ * 
+ * You can obtain a copy of the License at 
+ * http://IdentityConnectors.dev.java.net/legal/license.txt
+ * See the License for the specific language governing permissions and limitations 
+ * under the License. 
+ * 
+ * When distributing the Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at identityconnectors/legal/license.txt.
+ * If applicable, add the following below this CDDL Header, with the fields 
+ * enclosed by brackets [] replaced by your own identifying information: 
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * ====================
+ */
+package org.identityconnectors.contract.test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.identityconnectors.common.logging.Log;
+import org.identityconnectors.framework.api.operations.APIOperation;
+import org.identityconnectors.framework.api.operations.CreateApiOp;
+import org.identityconnectors.framework.api.operations.DeleteApiOp;
+import org.identityconnectors.framework.api.operations.GetApiOp;
+import org.identityconnectors.framework.common.objects.Attribute;
+import org.identityconnectors.framework.common.objects.AttributeBuilder;
+import org.identityconnectors.framework.common.objects.ConnectorObject;
+import org.identityconnectors.framework.common.objects.ObjectClass;
+import org.identityconnectors.framework.common.objects.Uid;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Contract test of {@link CreateApiOp} operation.
+ */
+@RunWith(Parameterized.class)
+public class CreateApiOpTests extends ObjectClassRunner {
+    /**
+     * Logging..
+     */
+    private static final Log LOG = Log.getLog(CreateApiOpTests.class);
+    private static final String TEST_NAME = "Create";
+
+    public CreateApiOpTests(ObjectClass oclass) {
+        super(oclass);
+    }
+
+    /**
+     * {@inheritDoc}     
+     */
+    @Override
+    public Class<? extends APIOperation> getAPIOperation() {
+        return CreateApiOp.class;
+    }
+        
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void testRun() {
+        
+        Uid uid = null;
+        
+        try {
+            Set<Attribute> attrs = ConnectorHelper.getCreateableAttributes(getDataProvider(),
+                    getObjectClassInfo(), getTestName(), 0, true, false);
+            
+            // should throw UnsupportedObjectClass if not supported
+            uid = getConnectorFacade().create(getObjectClass(), attrs,
+                    getOperationOptionsByOp(CreateApiOp.class));            
+
+            // get the user to make sure it exists now
+            ConnectorObject obj = getConnectorFacade().getObject(getObjectClass(), uid,
+                    getOperationOptionsByOp(GetApiOp.class));
+
+            assertNotNull("Unable to retrieve newly created object", obj);
+
+            // compare requested attributes to retrieved attributes
+            ConnectorHelper.checkObject(getObjectClassInfo(), obj, attrs);
+        } finally {
+            if (uid != null) {
+                // delete the object
+                getConnectorFacade().delete(getSupportedObjectClass(), uid,
+                        getOperationOptionsByOp(DeleteApiOp.class));
+            }
+        }
+    }
+    
+    /**
+     * Tests create method with invalid Attribute, RuntimeException is expected
+     */
+    @Test
+    public void testCreateFailUnsupportedAttribute() {
+        // run the contract test only if create is supported by tested object class
+        if (ConnectorHelper.operationSupported(getConnectorFacade(), getObjectClass(),
+                getAPIOperation())) {
+            // create not supported Attribute Set
+            Set<Attribute> attrs = new HashSet<Attribute>();
+            attrs.add(AttributeBuilder.build("NONEXISTINGATTRIBUTE"));
+
+            try {
+                // do the create call
+                // note - the ObjectClassInfo is always supported
+                getConnectorFacade().create(getObjectClass(), attrs, null);
+            }
+            catch (RuntimeException ex) {
+                // ok
+            }
+        }
+        else {
+            LOG.info("----------------------------------------------------------------------------------------");
+            LOG.info("Skipping test ''testCreateFailUnsupportedAttribute'' for object class ''{0}''.", getObjectClass());
+            LOG.info("----------------------------------------------------------------------------------------");
+        }
+    }
+    
+    /**
+     * Tests create twice with the same attributes. It should return different
+     * Uids.
+     */
+    @Test
+    public void testCreateWithSameAttributes() {
+        // run the contract test only if create is supported by tested object class
+        if (ConnectorHelper.operationSupported(getConnectorFacade(), getObjectClass(), getAPIOperation())) {
+            Uid uid1 = null;
+            Uid uid2 = null;
+
+            try {
+                Set<Attribute> attrs = ConnectorHelper.getCreateableAttributes(getDataProvider(),
+                        getObjectClassInfo(), getTestName(), 1, true, false);
+
+                // ObjectClassInfo is always supported
+                uid1 = getConnectorFacade().create(getSupportedObjectClass(), attrs,
+                        getOperationOptionsByOp(CreateApiOp.class));
+
+                // get the object to make sure it exist now
+                ConnectorObject obj1 = getConnectorFacade().getObject(getSupportedObjectClass(),
+                        uid1, getOperationOptionsByOp(GetApiOp.class));
+                assertNotNull("Unable to retrieve newly created object", obj1);
+
+                // compare requested attributes to retrieved attributes
+                ConnectorHelper.checkObject(getObjectClassInfo(), obj1, attrs);
+
+                /* SECOND CREATE: */
+
+                // should return different uid or throw
+                uid2 = getConnectorFacade().create(getSupportedObjectClass(), attrs, getOperationOptionsByOp(CreateApiOp.class));
+                assertFalse("Create returned the same Uid as by previous create.", uid1
+                        .equals(uid2));
+
+                // get the object to make sure it exists now
+                ConnectorObject obj2 = getConnectorFacade().getObject(getSupportedObjectClass(),
+                        uid2, getOperationOptionsByOp(GetApiOp.class));
+                assertNotNull("Unable to retrieve newly created object", obj2);
+
+                // compare requested attributes to retrieved attributes
+                ConnectorHelper.checkObject(getObjectClassInfo(), obj2, attrs);
+            } catch (RuntimeException ex) {
+                // ok - second create could throw this exception
+            } finally {
+                if (uid1 != null) {
+                    // delete the object
+                    ConnectorHelper.deleteObject(getConnectorFacade(), getSupportedObjectClass(), uid1,
+                            false, getOperationOptionsByOp(DeleteApiOp.class));
+                }
+                if (uid2 != null) {
+                    // delete the object
+                    ConnectorHelper.deleteObject(getConnectorFacade(), getSupportedObjectClass(), uid2,
+                            false, getOperationOptionsByOp(DeleteApiOp.class));
+                }
+            }
+        }
+        else {
+            LOG.info("----------------------------------------------------------------------------------------");
+            LOG.info("Skipping test ''testCreateWithSameAttributes'' for object class ''{0}''.", getObjectClass());
+            LOG.info("----------------------------------------------------------------------------------------");
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getTestName() {
+        return TEST_NAME;
+    }
+       
+}
