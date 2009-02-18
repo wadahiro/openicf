228c52808b3c1a37fcdcd7a555c3773768e46137
george_hetrick <george_hetrick@05b3e5af-d696-470f-a577-fd7599f74d3c>
2009-02-18 16:23:36 +0000

    Remove connection pooling from inside RW3270-based connectors. That needs to be done via connector pooling at the application level.
    git-svn-id: https://svn.forgerock.org/openicf/trunk/projects/bundles/3270hod@3613 05b3e5af-d696-470f-a577-fd7599f74d3c

--- src/org/identityconnectors/rw3270/hod/HodConnectionTests.java
+++ src/org/identityconnectors/rw3270/hod/HodConnectionTests.java
@@ -45,10 +45,8 @@ import org.identityconnectors.framework.common.objects.ConnectorObject;
 import org.identityconnectors.framework.common.objects.ResultsHandler;
 import org.identityconnectors.framework.spi.AbstractConfiguration;
 import org.identityconnectors.patternparser.MapTransform;
-import org.identityconnectors.rw3270.ConnectionPool;
-import org.identityconnectors.rw3270.PoolableConnectionConfiguration;
+import org.identityconnectors.rw3270.RW3270Configuration;
 import org.identityconnectors.rw3270.RW3270Connection;
-import org.identityconnectors.rw3270.PoolableConnectionFactory.ConnectionInfo;
 import org.identityconnectors.test.common.TestHelpers;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -58,7 +56,7 @@ import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
 
-public class HodConnectionPoolTests {
+public class HodConnectionTests {
 
     // Connector Configuration information
     //
@@ -88,9 +86,7 @@ public class HodConnectionPoolTests {
     public void testTelnetConnectionViaPool() {
         OurConfiguration configuration = createConfiguration();
         try {
-            ConnectionPool pool = new ConnectionPool(configuration);
-            ConnectionInfo info = (ConnectionInfo)pool.borrowObject("TODO");
-            RW3270Connection connection = info.getConnection();
+            RW3270Connection connection = new HodConnection(configuration);
             try {
                 // Now, display a user
                 //
@@ -98,7 +94,6 @@ public class HodConnectionPoolTests {
                 String line = executeCommand(connection, command);
                 Assert.assertTrue(line.contains("USER=IDM03"));
                 System.out.println(line);
-                pool.returnObject("TODO", info);
             } finally {
                 connection.dispose();
             }
@@ -164,9 +159,7 @@ public class HodConnectionPoolTests {
         
         OurConfiguration configuration = createConfiguration();
         try {
-            ConnectionPool pool = new ConnectionPool(configuration);
-            ConnectionInfo info = (ConnectionInfo)pool.borrowObject("TODO");
-            RW3270Connection connection = info.getConnection();
+            RW3270Connection connection = new HodConnection(configuration);
             try {
                 // Now, display a user's OMVS info
                 //
@@ -182,7 +175,6 @@ public class HodConnectionPoolTests {
                     Assert.assertNotNull(attributes.get("TSO.USERDATA"));
                     Assert.assertNotNull(attributes.get("TSO.JOBCLASS"));
                 }
-                pool.returnObject("TODO", info);
             } finally {
                 connection.dispose();
             }
@@ -208,9 +200,7 @@ public class HodConnectionPoolTests {
             "</MapTransform>";
         
         try {
-            ConnectionPool pool = new ConnectionPool(configuration);
-            ConnectionInfo info = (ConnectionInfo)pool.borrowObject("TODO");
-            RW3270Connection connection = info.getConnection();
+            RW3270Connection connection = new HodConnection(configuration);
             try {
                 // Now, display a user's CICS info
                 //
@@ -221,7 +211,6 @@ public class HodConnectionPoolTests {
                 Map<String, Object> attributes = (Map<String, Object>)transform.transform(line);
                 Assert.assertNotNull(attributes.get("CICS.XRFSOFF"));
                 Assert.assertTrue(attributes.get("CICS.OPCLASS") instanceof List);
-                pool.returnObject("TODO", info);
             } finally {
                 connection.dispose();
             }
@@ -251,9 +240,7 @@ public class HodConnectionPoolTests {
             "</MapTransform>";
         
         try {
-            ConnectionPool pool = new ConnectionPool(configuration);
-            ConnectionInfo info = (ConnectionInfo)pool.borrowObject("TODO");
-            RW3270Connection connection = info.getConnection();
+            RW3270Connection connection = new HodConnection(configuration);
             try {
                 // Now, display a user's TSO info
                 //
@@ -283,9 +270,9 @@ public class HodConnectionPoolTests {
         config.setUseSsl(USE_SSL);
         config.setConnectScript(getLoginScript());
         config.setDisconnectScript(getLogoffScript());
-        config.setUserNames(new String[] { SYSTEM_USER });
-        config.setPasswords(new GuardedString[] { new GuardedString(SYSTEM_PASSWORD.toCharArray()) });
-        config.setPoolNames(new String[] { "TODO" });
+        config.setUserName(SYSTEM_USER );
+        config.setPassword(new GuardedString(SYSTEM_PASSWORD.toCharArray()));
+        config.setScriptingLanguage("GROOVY");
         config.setEvictionInterval(60000);
         config.setConnectionClassName(HodConnection.class.getName());
 
@@ -327,10 +314,10 @@ public class HodConnectionPoolTests {
     }
 
     private String getLogoffScript() {
-        String script =
-            "connection.send(\"LOGOFF[enter]\");\n" +
-            "connection.waitFor(\"=====>\", SHORT_WAIT);\n" +
-            "connection.dispose();\n";
+        String script = "connection.send(\"LOGOFF[enter]\");\n";
+//            "connection.send(\"LOGOFF[enter]\");\n" +
+//            "connection.waitFor(\"=====>\", SHORT_WAIT);\n" +
+//            "connection.dispose();\n";
         return script;
     }
 
@@ -351,14 +338,14 @@ public class HodConnectionPoolTests {
         }
     }
     
-    public static class OurConfiguration extends AbstractConfiguration implements PoolableConnectionConfiguration {
+    public static class OurConfiguration extends AbstractConfiguration implements RW3270Configuration {
         private String _connectScript;
         private String _disconnectScript;
         private String _host;
         private Integer _port;
-        private GuardedString[] _passwords;
-        private String[] _poolNames;
-        private String[] _userNames;
+        private GuardedString _password;
+        private String _language;
+        private String _userName;
         private Integer _evictionInterval;
         private String _connectClass;
         private Boolean _useSsl ;
@@ -383,20 +370,16 @@ public class HodConnectionPoolTests {
             return _port;
         }
 
-        public GuardedString[] getPasswords() {
-            return _passwords;
-        }
-
-        public String[] getPoolNames() {
-            return _poolNames;
+        public GuardedString getPassword() {
+            return _password;
         }
 
         public Boolean getUseSsl() {
             return _useSsl;
         }
 
-        public String[] getUserNames() {
-            return _userNames;
+        public String getUserName() {
+            return _userName;
         }
 
         public void setConnectScript(String script) {
@@ -419,32 +402,36 @@ public class HodConnectionPoolTests {
             _port = port;
         }
 
-        public void setPasswords(GuardedString[] passwords) {
-            _passwords = passwords;
-        }
-
-        public void setPoolNames(String[] poolNames) {
-            _poolNames = poolNames;
+        public void setPassword(GuardedString password) {
+            _password = password;
         }
 
         public void setUseSsl(Boolean useSsl) {
             _useSsl = useSsl;
         }
 
-        public void setUserNames(String[] userNames) {
-            _userNames = userNames;
+        public void setUserName(String userName) {
+            _userName = userName;
         }
-        
+
         public Integer getEvictionInterval() {
             return _evictionInterval;
         }
-        
+
         public void setEvictionInterval(Integer interval) {
             _evictionInterval = interval;
         }
-        
+
         public void validate() {
-            
+
+        }
+
+        public String getScriptingLanguage() {
+            return _language;
+        }
+
+        public void setScriptingLanguage(String language) {
+            _language = language;
         }
     }
     
