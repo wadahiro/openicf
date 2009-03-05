9433081a827beaf91112e0504c0967c54531bfdb
icbuilders <icbuilders@05b3e5af-d696-470f-a577-fd7599f74d3c>
2009-03-05 15:49:42 +0000

    Tagged by Hudson svn-tag plugin. Build:hudson-org.identityconnectors.ldap-91.
    git-svn-id: https://svn.forgerock.org/openicf/tags/last-successful@4355 05b3e5af-d696-470f-a577-fd7599f74d3c

---
+++ org.identityconnectors.ldap/src/org/identityconnectors/ldap/LdapConnectionTests.java
@@ -0,0 +1,128 @@
+/*
+ * ====================
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 2007-2008 Sun Microsystems, Inc. All rights reserved.     
+ * 
+ * The contents of this file are subject to the terms of the Common Development 
+ * and Distribution License("CDDL") (the "License").  You may not use this file 
+ * except in compliance with the License.
+ * 
+ * You can obtain a copy of the License at 
+ * http://IdentityConnectors.dev.java.net/legal/license.txt
+ * See the License for the specific language governing permissions and limitations 
+ * under the License. 
+ * 
+ * When distributing the Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at identityconnectors/legal/license.txt.
+ * If applicable, add the following below this CDDL Header, with the fields 
+ * enclosed by brackets [] replaced by your own identifying information: 
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * ====================
+ */
+package org.identityconnectors.ldap;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import javax.naming.Context;
+import javax.naming.NamingException;
+
+import org.identityconnectors.common.security.GuardedString;
+import org.junit.Test;
+
+import com.sun.jndi.ldap.ctl.PagedResultsControl;
+import com.sun.jndi.ldap.ctl.VirtualListViewControl;
+
+public class LdapConnectionTests extends LdapConnectorTestBase {
+
+    @Override
+    protected boolean restartServerAfterEachTest() {
+        return false;
+    }
+
+    @Test
+    public void testSupportedControls() {
+        LdapConnection conn = new LdapConnection(newConfiguration());
+        assertTrue(conn.supportsControl(PagedResultsControl.OID));
+        assertTrue(conn.supportsControl(VirtualListViewControl.OID));
+    }
+
+    @Test
+    public void testDefaultAuthenticationMethodIsInferred() throws NamingException {
+        LdapConfiguration config = newConfiguration();
+        config.setAuthentication(null);
+        config.setPrincipal(null);
+        LdapConnection conn = new LdapConnection(config);
+        assertEquals("none", conn.getInitialContext().getEnvironment().get(Context.SECURITY_AUTHENTICATION));
+
+        config = newConfiguration();
+        config.setAuthentication(null);
+        config.setPrincipal(ADMIN_DN);
+        config.setCredentials(ADMIN_PASSWORD);
+        conn = new LdapConnection(config);
+        assertEquals("simple", conn.getInitialContext().getEnvironment().get(Context.SECURITY_AUTHENTICATION));
+    }
+
+    @Test
+    public void testTest() {
+        LdapConfiguration config = newConfiguration();
+        config.setPort(4242);
+        LdapConnection conn = new LdapConnection(config);
+        try {
+            conn.test();
+            fail();
+        } catch (RuntimeException e) {
+            // Expected.
+        }
+
+        config = newConfiguration();
+        config.setHost("invalid");
+        conn = new LdapConnection(config);
+        try {
+            conn.test();
+            fail();
+        } catch (RuntimeException e) {
+            // Expected.
+        }
+
+        config = newConfiguration();
+        config.setPrincipal("uid=nobody");
+        conn = new LdapConnection(config);
+        try {
+            conn.test();
+            fail();
+        } catch (RuntimeException e) {
+            // Expected.
+        }
+
+        config = newConfiguration();
+        config.setCredentials(new GuardedString("bogus".toCharArray()));
+        conn = new LdapConnection(config);
+        try {
+            conn.test();
+            fail();
+        } catch (RuntimeException e) {
+            // Expected.
+        }
+
+        config = newConfiguration();
+        conn = new LdapConnection(config);
+        conn.test();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void testCheckAlive() {
+        LdapConfiguration config = newConfiguration();
+        config.setReadSchema(true); // Since we are calling createNativeSchema() below.
+        LdapConnection conn = new LdapConnection(config);
+        conn.checkAlive();
+        // Ensure the connection is really connected to the server.
+        conn.createNativeSchema();
+        conn.checkAlive();
+        stopServer();
+        // This should throw RuntimeException.
+        conn.checkAlive();
+    }
+}
---
+++ org.identityconnectors.ldap/src/org/identityconnectors/ldap/schema/LdapSchemaMappingTests.java
@@ -0,0 +1,148 @@
+/*
+ * ====================
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ * 
+ * Copyright 2007-2008 Sun Microsystems, Inc. All rights reserved.     
+ * 
+ * The contents of this file are subject to the terms of the Common Development 
+ * and Distribution License("CDDL") (the "License").  You may not use this file 
+ * except in compliance with the License.
+ * 
+ * You can obtain a copy of the License at 
+ * http://IdentityConnectors.dev.java.net/legal/license.txt
+ * See the License for the specific language governing permissions and limitations 
+ * under the License. 
+ * 
+ * When distributing the Covered Code, include this CDDL Header Notice in each file
+ * and include the License file at identityconnectors/legal/license.txt.
+ * If applicable, add the following below this CDDL Header, with the fields 
+ * enclosed by brackets [] replaced by your own identifying information: 
+ * "Portions Copyrighted [year] [name of copyright owner]"
+ * ====================
+ */
+package org.identityconnectors.ldap.schema;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.EnumSet;
+import java.util.Set;
+
+import org.identityconnectors.framework.api.operations.AuthenticationApiOp;
+import org.identityconnectors.framework.common.objects.AttributeInfo;
+import org.identityconnectors.framework.common.objects.AttributeInfoBuilder;
+import org.identityconnectors.framework.common.objects.AttributeInfoUtil;
+import org.identityconnectors.framework.common.objects.ObjectClass;
+import org.identityconnectors.framework.common.objects.ObjectClassInfo;
+import org.identityconnectors.framework.common.objects.Schema;
+import org.identityconnectors.framework.common.objects.AttributeInfo.Flags;
+import org.identityconnectors.ldap.LdapConfiguration;
+import org.identityconnectors.ldap.LdapConnectorTestBase;
+import org.junit.Test;
+
+public class LdapSchemaMappingTests extends LdapConnectorTestBase {
+
+    // TODO operational attributes.
+    // TODO test for operation option infos.
+
+    @Override
+    protected boolean restartServerAfterEachTest() {
+        return false;
+    }
+
+    @Test
+    public void testObjectClassAttrIsReadOnly() {
+        LdapConfiguration config = newConfiguration();
+        config.setReadSchema(true);
+        Schema schema = newFacade(config).schema();
+        for (ObjectClassInfo oci : schema.getObjectClassInfo()) {
+            AttributeInfo attrInfo = AttributeInfoUtil.find("objectClass", oci.getAttributeInfo());
+            assertFalse(attrInfo.isRequired());
+            assertFalse(attrInfo.isCreateable());
+            assertFalse(attrInfo.isUpdateable());
+        }
+    }
+
+    @Test
+    public void testAccountSchema() {
+        LdapConfiguration config = newConfiguration();
+        config.setReadSchema(true);
+        Schema schema = newFacade(config).schema();
+
+        ObjectClassInfo oci = schema.findObjectClassInfo(ObjectClass.ACCOUNT_NAME);
+        assertFalse(oci.isContainer());
+
+        Set<AttributeInfo> attrInfos = oci.getAttributeInfo();
+
+        AttributeInfo info = AttributeInfoUtil.find("cn", attrInfos);
+        assertEquals(AttributeInfoBuilder.build("cn", String.class, EnumSet.of(Flags.REQUIRED, Flags.MULTIVALUED)), info);
+
+        info = AttributeInfoUtil.find("uid", attrInfos);
+        assertEquals(AttributeInfoBuilder.build("uid", String.class, EnumSet.of(Flags.MULTIVALUED)), info);
+
+        info = AttributeInfoUtil.find("givenName", attrInfos);
+        assertEquals(AttributeInfoBuilder.build("givenName", String.class, EnumSet.of(Flags.MULTIVALUED)), info);
+
+        info = AttributeInfoUtil.find("sn", attrInfos);
+        assertEquals(AttributeInfoBuilder.build("sn", String.class, EnumSet.of(Flags.REQUIRED, Flags.MULTIVALUED)), info);
+    }
+
+    @Test
+    public void testGroupSchema() {
+        LdapConfiguration config = newConfiguration();
+        config.setReadSchema(true);
+        Schema schema = newFacade(config).schema();
+
+        ObjectClassInfo oci = schema.findObjectClassInfo(ObjectClass.GROUP_NAME);
+        assertFalse(oci.isContainer());
+
+        Set<AttributeInfo> attrInfos = oci.getAttributeInfo();
+
+        AttributeInfo info = AttributeInfoUtil.find("cn", attrInfos);
+        assertEquals(AttributeInfoBuilder.build("cn", String.class, EnumSet.of(Flags.REQUIRED, Flags.MULTIVALUED)), info);
+    }
+
+    @Test
+    public void testAuthenticationOnlyForAccounts() {
+        LdapConfiguration config = newConfiguration();
+        config.setReadSchema(true);
+        Schema schema = newFacade(config).schema();
+        Set<ObjectClassInfo> ocis = schema.getSupportedObjectClassesByOperation().get(AuthenticationApiOp.class);
+        assertEquals(1, ocis.size());
+        assertTrue(ocis.iterator().next().is(ObjectClass.ACCOUNT_NAME));
+    }
+
+    @Test
+    public void testExtendedObjectClasses() {
+        Schema schema = newFacade(newConfiguration("dNSDomain:dc")).schema();
+
+        ObjectClassInfo dnsDomainInfo = schema.findObjectClassInfo("dNSDomain");
+        Set<AttributeInfo> dnsDomainAttrInfos = dnsDomainInfo .getAttributeInfo();
+
+        // Inherited from domain.
+        AttributeInfo dcInfo = AttributeInfoUtil.find("telephoneNumber", dnsDomainAttrInfos );
+        assertEquals(AttributeInfoBuilder.build("telephoneNumber", String.class, EnumSet.of(Flags.MULTIVALUED)), dcInfo);
+        // Defined in dNSDomain.
+        AttributeInfo mxRecordInfo = AttributeInfoUtil.find("MXRecord", dnsDomainAttrInfos );
+        assertEquals(AttributeInfoBuilder.build("MXRecord", String.class, EnumSet.of(Flags.MULTIVALUED)), mxRecordInfo);
+    }
+
+    @Test
+    public void testAttributeTypes() {
+        LdapConfiguration config = newConfiguration();
+        config.setReadSchema(true);
+        Schema schema = newFacade(config).schema();
+        ObjectClassInfo accountInfo = schema.findObjectClassInfo(ObjectClass.ACCOUNT_NAME);
+        Set<AttributeInfo> accountAttrInfos = accountInfo.getAttributeInfo();
+
+        assertEquals(String.class, AttributeInfoUtil.find("cn", accountAttrInfos).getType());
+        assertEquals(String.class, AttributeInfoUtil.find("ou", accountAttrInfos).getType());
+        assertEquals(String.class, AttributeInfoUtil.find("telephoneNumber", accountAttrInfos).getType());
+
+        assertEquals(byte[].class, AttributeInfoUtil.find("audio", accountAttrInfos).getType());
+        assertEquals(byte[].class, AttributeInfoUtil.find("jpegPhoto", accountAttrInfos).getType());
+        assertEquals(byte[].class, AttributeInfoUtil.find("userCertificate", accountAttrInfos).getType());
+        assertEquals(byte[].class, AttributeInfoUtil.find("x500UniqueIdentifier", accountAttrInfos).getType());
+    }
+}
