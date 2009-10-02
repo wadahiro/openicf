8ba3a171992d77400828bddde9599dd95c0702e5
roman_kitko <roman_kitko@05b3e5af-d696-470f-a577-fd7599f74d3c>
2009-10-02 17:17:15 +0000

    Small changes in pom files
    git-svn-id: https://svn.forgerock.org/openicf/branches/maven-proposal@5348 05b3e5af-d696-470f-a577-fd7599f74d3c

--- projects/maven/maven-surefire-execs-plugin/src/main/java/org/apache/maven/plugin/surefire/SurefireExecsPlugin.java
+++ projects/maven/maven-surefire-execs-plugin/src/main/java/org/apache/maven/plugin/surefire/SurefireExecsPlugin.java
@@ -1,24 +1,5 @@
 package org.apache.maven.plugin.surefire;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 import java.io.File;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
@@ -38,6 +19,26 @@ import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
+
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 
 /**
  * Run tests using Surefire.
@@ -513,10 +514,17 @@ public class SurefireExecsPlugin
     public void execute()
         throws MojoExecutionException, MojoFailureException
     {
+        if ( skip || skipTests || skipExec )
+        {
+            getLog().info( "Tests are skipped." );
+            return ;
+        }
+    	
+    	
     	if(testExecutionsPropertyValues != null && testExecutions != null && !testExecutions.isEmpty()){
     		throw new MojoExecutionException("Cannot set both testExecutionsPropertyValues and testExecutions");
     	}
-    	if(testExecutionsPropertyValues == null && (testExecutions == null || testExecutions.isEmpty())){
+    	if((testExecutionsPropertyValues == null || testExecutionsPropertyValues.propertyValues == null) && (testExecutions == null || testExecutions.isEmpty())){
     		throw new MojoExecutionException("Both testExecutionsPropertyValues and testExecutions are not set");
     	}
     	
@@ -529,6 +537,10 @@ public class SurefireExecsPlugin
     			execution.systemProperties.setProperty(testExecutionsPropertyValues.propertyName, propertyValue);
     			testExecutions.add(execution);
     		}
+    	}
+    	
+    	if(testExecutions == null){
+    		getLog().warn("No Test executions defined");
     	}
     	
     	for(TestExecution execution : testExecutions){
