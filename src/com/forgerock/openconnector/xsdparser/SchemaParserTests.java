f1c79f33c6dc2594164149c573d86126a52e521f
olaschr <olaschr@05b3e5af-d696-470f-a577-fd7599f74d3c>
2011-02-10 10:15:17 +0000

    git-svn-id: https://svn.forgerock.org/openicf/branches/XML@5865 05b3e5af-d696-470f-a577-fd7599f74d3c

---
+++ src/com/forgerock/openconnector/xsdparser/SchemaParserTests.java
@@ -0,0 +1,58 @@
+
+package com.forgerock.openconnector.xsdparser;
+
+
+import com.forgerock.openconnector.xml.XMLConnector;
+import org.identityconnectors.framework.common.exceptions.ConnectorIOException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class SchemaParserTests {
+    SchemaParser parser;
+
+
+    @Before
+    public void setUp(){
+        parser = new SchemaParser(XMLConnector.class, "test/xml_store/ef2bc95b-76e0-48e2-86d6-4d4f44d4e4a4.xsd");
+    }
+
+    @After
+    public void tearDown(){
+        parser = null;
+    }
+
+    @Test
+    public void newSchemaParserShouldReturnNewSchemaParser(){
+        SchemaParser parserTest = new SchemaParser(XMLConnector.class, "test/xml_store/ef2bc95b-76e0-48e2-86d6-4d4f44d4e4a4.xsd");
+        assertNotNull(parserTest);
+    }
+
+    @Test(expected= ConnectorIOException.class)
+    public void newSchemaParserWithInvalidFilePathShouldReturnConnectorIOException(){
+        SchemaParser parserTest = new SchemaParser(XMLConnector.class, "test/xml_store/404");
+        
+    }
+    @Test(expected= IllegalArgumentException.class)
+    public void newSchemaParserWithEmptyFilePathShouldReturnIllegalArgumentException(){
+        SchemaParser parserTest = new SchemaParser(XMLConnector.class, "");
+
+    }
+    @Test(expected= NullPointerException.class)
+    public void newSchemaParserWithNullClassShouldReturnNullPointerException(){
+        SchemaParser parserTest = new SchemaParser(null, "sfsdf");
+
+    }
+
+    @Test
+    public void getXsdSchemaShouldReturnXsdSchemaSet(){
+        assertNotNull(parser.getXsdSchema());
+    }
+
+    @Test
+    public void parseSchemaShouldReturnSchema(){
+        assertNotNull(parser.parseSchema());
+    }
+}
