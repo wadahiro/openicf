268a892e7771a091c87152fafcf3ef795492d658
olaschr <olaschr@05b3e5af-d696-470f-a577-fd7599f74d3c>
2011-02-10 12:31:24 +0000

    git-svn-id: https://svn.forgerock.org/openicf/branches/XML@5871 05b3e5af-d696-470f-a577-fd7599f74d3c

--- src/com/forgerock/openconnector/xsdparser/SchemaParser.java
+++ src/com/forgerock/openconnector/xsdparser/SchemaParser.java
@@ -196,6 +196,7 @@ public class SchemaParser {
             String eMessage =  "Failed to parser XSD-schema from file: " + filePath;
             log.error(e, eMessage);
             throw new ConnectorIOException(filePath, e);
+            
         } catch (IOException e) {
             String eMessage =  "Failed to read from file: " + filePath;
             log.error(e, eMessage);
