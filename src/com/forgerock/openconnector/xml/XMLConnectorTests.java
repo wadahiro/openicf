f1bc3771be47eacafcbba01f7aaf2b738bba357d
olaschr <olaschr@05b3e5af-d696-470f-a577-fd7599f74d3c>
2011-02-16 09:37:49 +0000

    git-svn-id: https://svn.forgerock.org/openicf/branches/XML@5937 05b3e5af-d696-470f-a577-fd7599f74d3c

--- src/com/forgerock/openconnector/xml/XMLConnectorTests.java
+++ src/com/forgerock/openconnector/xml/XMLConnectorTests.java
@@ -1,6 +1,5 @@
 package com.forgerock.openconnector.xml;
 
-
 import java.util.HashSet;
 import java.util.Set;
 import org.identityconnectors.common.security.GuardedString;
@@ -12,20 +11,19 @@ import org.identityconnectors.framework.common.objects.Uid;
 import org.junit.*;
 import static org.junit.Assert.*;
 
-
 public class XMLConnectorTests {
 
     private static XMLConnector xmlConnector;
     private static XMLConfiguration xmlConfig;
-
-    private final static String NAME = "Name";
+    private final static String ACCOUNT_NAME = "Name";
+    private final static String GROUP_NAME = "This is name";
 
     @BeforeClass
     public static void setUp() {
         xmlConfig = new XMLConfiguration();
         xmlConfig.setXmlFilePath("test/xml_store/testXmlConnector.xml");
         xmlConfig.setXsdFilePath("test/xml_store/ef2bc95b-76e0-48e2-86d6-4d4f44d4e4a4.xsd");
-        
+
         xmlConnector = new XMLConnector();
     }
 
@@ -36,87 +34,142 @@ public class XMLConnectorTests {
     }
 
     @Test
-    public void callInitShouldNotCastException(){
+    public void initShouldNotCastException() {
         xmlConnector.init(xmlConfig);
     }
 
     @Test
-    public void callTestShouldNotCastException(){
+    public void testShouldNotCastException() {
         xmlConnector.test();
     }
 
-    @Test(expected=NullPointerException.class)
-    public void callTestShouldCastNullPointerException(){
+    @Test(expected = NullPointerException.class)
+    public void testShouldCastNullPointerException() {
         XMLConnector xmlCon = new XMLConnector();
         xmlCon.test();
     }
 
-    @Test(expected=IllegalArgumentException.class)
-    public void callTestShouldCastIllegalArgumentException(){
+    @Test(expected = IllegalArgumentException.class)
+    public void testShouldCastIllegalArgumentException() {
         XMLConfiguration conf = new XMLConfiguration();
-        
+
         conf.setXmlFilePath("404.xml");
         conf.setXsdFilePath("test/xml_store/ef2bc95b-76e0-48e2-86d6-4d4f44d4e4a4.xsd");
         conf.setXsdIcfFilePath("404.xml");
 
         XMLConnector xmlCon = new XMLConnector();
         xmlCon.init(conf);
-        
+
         xmlCon.test();
     }
 
     @Test
-    public void schemaShouldReturnSchema(){
+    public void schemaShouldReturnSchema() {
         assertNotNull(xmlConnector.schema());
     }
 
     @Test
-    public void creatFilterTranslatorShouldReturnNewXmlFilterTranslator(){
+    public void creatFilterTranslatorShouldReturnNewXmlFilterTranslator() {
         assertNotNull(xmlConnector.createFilterTranslator(ObjectClass.ACCOUNT, null));
     }
 
     @Test
-    public void executeQueryShouldNotCastException(){
-        
+    public void getConfigurationShouldReturnConfiguration() {
+        assertNotNull(xmlConnector.getConfiguration());
     }
 
     @Test
-    public void createAccountShouldReturnUid(){
-        Uid uid = xmlConnector.create(ObjectClass.ACCOUNT, creatAttributes(), null);
+    public void executeQueryShouldNotCastException() {
+    }
+
+    @Test
+    public void createAccountShouldReturnUid() {
+        Uid uid = xmlConnector.create(ObjectClass.ACCOUNT, createAttributesAccount(), null);
         assertNotNull(uid);
     }
 
-    @Test(expected=AlreadyExistsException.class)
-    public void createAccountWithSameNameShouldCastAlreadyExistsException(){
-        Uid uid = xmlConnector.create(ObjectClass.ACCOUNT, creatAttributes(), null);
+    @Test(expected = AlreadyExistsException.class)
+    public void createAccountWithSameNameShouldCastAlreadyExistsException() {
+        xmlConnector.create(ObjectClass.ACCOUNT, createAttributesAccount(), null);
+    }
+
+    @Test
+    public void createGroupShouldReturnUid() {
+        Uid uid = xmlConnector.create(ObjectClass.GROUP, createAttributesGroup(), null);
+        assertNotNull(uid);
+    }
+
+    @Test(expected = AlreadyExistsException.class)
+    public void createGroupWithSameNameShouldCastAlreadyExistsException() {
+        xmlConnector.create(ObjectClass.GROUP, createAttributesGroup(), null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void createEntityWithNullAttributesShouldCastNullPointerException() {
+        xmlConnector.create(null, null, null);
+    }
+
+    @Test
+    public void updateAccountShouldReturnUid() {
+        Set<Attribute> attributes = createAttributesAccount();
+
+        attributes.add(AttributeBuilder.build("email", "mailadress@company.org"));
+        attributes.add(AttributeBuilder.build("__ENABLE__", true));
+
+        Uid uid = xmlConnector.update(ObjectClass.ACCOUNT, new Uid(ACCOUNT_NAME), attributes, null);
+
         assertNotNull(uid);
     }
 
     @Test
-    public void updateAccountShouldReturnUid(){
-        Set<Attribute> set = creatAttributes();
+    public void updateGroupShouldReturnUid() {
+        Set<Attribute> attributes = new HashSet<Attribute>();
 
-        set.add(AttributeBuilder.build("email","mailadress@company.org"));
-        set.add(AttributeBuilder.build("__ENABLE__", true));
+        attributes.add(AttributeBuilder.build("__DESCRIPTION__", "this is description updated"));
+        attributes.add(AttributeBuilder.build("__SHORT_NAME__", "tidu"));
 
-        Uid uid = xmlConnector.update(ObjectClass.ACCOUNT, new Uid(NAME), set, null);
-
+        Uid uid = xmlConnector.update(ObjectClass.GROUP, new Uid(GROUP_NAME), attributes, null);
         assertNotNull(uid);
     }
 
-    @Test
-    public void deleteAccountShouldNotCastException(){
-        xmlConnector.delete(ObjectClass.ACCOUNT, new Uid(NAME), null);
+    @Test(expected = NullPointerException.class)
+    public void updateEntryWithNullShouldCastNullPointerException() {
+        xmlConnector.update(null, null, null, null);
     }
 
-    private Set<Attribute> creatAttributes(){
+    @Test
+    public void deleteAccountShouldNotCastException() {
+        xmlConnector.delete(ObjectClass.ACCOUNT, new Uid(ACCOUNT_NAME), null);
+    }
+
+    @Test
+    public void deleteGroupShouldNotCastException() {
+        xmlConnector.delete(ObjectClass.GROUP, new Uid(GROUP_NAME), null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void deleteAccountWithNullShouldCastNullPointerException() {
+        xmlConnector.delete(null, null, null);
+    }
+
+    private Set<Attribute> createAttributesAccount() {
         Set<Attribute> set = new HashSet<Attribute>();
 
-        set.add(AttributeBuilder.build("__NAME__", NAME));
+        set.add(AttributeBuilder.build("__NAME__", ACCOUNT_NAME));
 
         char[] chars = {'A', 'B', 'C', 'D'};
         set.add(AttributeBuilder.buildPassword(new GuardedString(chars)));
         set.add(AttributeBuilder.build("address", "Adressroad 12"));
         return set;
+    }
+
+    private Set<Attribute> createAttributesGroup() {
+        Set<Attribute> attributes = new HashSet<Attribute>();
+
+        attributes.add(AttributeBuilder.build("__NAME__", GROUP_NAME));
+        attributes.add(AttributeBuilder.build("__DESCRIPTION__", "this is description"));
+        attributes.add(AttributeBuilder.build("__SHORT_NAME__", "tid"));
+
+        return attributes;
     }
 }
