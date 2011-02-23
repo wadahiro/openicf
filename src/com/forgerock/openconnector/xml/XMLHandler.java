84b7bbe83403b9e4a76a3d0d2c443aa4e44e105a
olaschr <olaschr@05b3e5af-d696-470f-a577-fd7599f74d3c>
2011-02-23 08:09:42 +0000

    git-svn-id: https://svn.forgerock.org/openicf/branches/XML@5987 05b3e5af-d696-470f-a577-fd7599f74d3c

--- src/com/forgerock/openconnector/xml/XMLHandler.java
+++ src/com/forgerock/openconnector/xml/XMLHandler.java
@@ -10,8 +10,10 @@ import java.util.Set;
 import org.identityconnectors.framework.common.exceptions.UnknownUidException;
 import org.identityconnectors.framework.common.objects.Attribute;
 import org.identityconnectors.framework.common.objects.ConnectorObject;
+import org.identityconnectors.framework.common.objects.Name;
 import org.identityconnectors.framework.common.objects.ObjectClass;
 import org.identityconnectors.framework.common.objects.Uid;
+import org.w3c.dom.Element;
 
 
 public interface XMLHandler {
@@ -23,6 +25,8 @@ public interface XMLHandler {
     public void delete(final ObjectClass objClass, final Uid uid) throws UnknownUidException;
 
     public Collection<ConnectorObject> search(String query, ObjectClass objectClass);
+
+    public Element getEntry(ObjectClass objectClass, Name name);
 
     public void serialize();
 }
