3ed7c43aebc0ae2ea838f3b98d0dcd457ce64ddb
olaschr <olaschr@05b3e5af-d696-470f-a577-fd7599f74d3c>
2011-03-01 14:44:19 +0000

    git-svn-id: https://svn.forgerock.org/openicf/branches/XML@6021 05b3e5af-d696-470f-a577-fd7599f74d3c

--- src/com/forgerock/openconnector/xsdparser/XSDAnnotationParser.java
+++ src/com/forgerock/openconnector/xsdparser/XSDAnnotationParser.java
@@ -1,3 +1,29 @@
+/*
+ *
+ * Copyright (c) 2010 ForgeRock Inc. All Rights Reserved
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://www.opensource.org/licenses/cddl1.php or
+ * OpenIDM/legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at OpenIDM/legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted 2010 [name of copyright owner]"
+ *
+ * $Id$
+ */
+
 package com.forgerock.openconnector.xsdparser;
 
 import org.xml.sax.Attributes;
@@ -16,98 +42,71 @@ public class XSDAnnotationParser extends AnnotationParser{
 	private String addValue = "";
 	
 	StringBuilder stringBuilder = new StringBuilder();
+
 	@Override
-	public ContentHandler getContentHandler(AnnotationContext arg0, String arg1, ErrorHandler arg2, EntityResolver arg3) {
-		return new ContentHandler() {
-			
-			@Override
-			public void startPrefixMapping(String arg0, String arg1)
-					throws SAXException {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {	
-				if(localName.equals("appinfo") || localName.equals("sequence")){
-					parse = true;
-				}
-				
-				if(!qName.equals("xsd:appinfo") && !qName.equals("xsd:element")){
-					addValue = qName;
-				}	
-			}
-			
-			@Override
-			public void startDocument() throws SAXException {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void skippedEntity(String arg0) throws SAXException {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void setDocumentLocator(Locator arg0) {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void processingInstruction(String arg0, String arg1)
-					throws SAXException {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void ignorableWhitespace(char[] arg0, int arg1, int arg2)
-					throws SAXException {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void endPrefixMapping(String arg0) throws SAXException {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void endElement(String uri, String localName, String qName) throws SAXException {
-				if(localName.equals("appinfo")){
-					parse = false;
-				}
-				
-			}
-			
-			@Override
-			public void endDocument() throws SAXException {
-				// TODO Auto-generated method stub
-				
-			}
-			
-			@Override
-			public void characters(char[] chars, int start, int length) throws SAXException {
-				if(parse){
-					StringBuilder sb = new StringBuilder();
-					sb.append(chars, start, length);
-					if(!sb.toString().replace(" ", "").trim().equals("")){
-						String stringToAppend = addValue + " " + sb.toString().trim();
-						stringBuilder.append(stringToAppend);
-						stringBuilder.append("\n");
-					}
-				}
-			}
-		};
+	public ContentHandler getContentHandler(AnnotationContext annotationContext, String parentElementName, ErrorHandler errorHandler, EntityResolver entryResolver) {
+            return new ContentHandler() {
+
+                @Override
+                public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+
+                    if(localName.equals("appinfo") || localName.equals("sequence")){
+                        parse = true;
+                    }
+
+                    if(!qName.equals("xsd:appinfo") && !qName.equals("xsd:element")){
+                        addValue = qName;
+                    }
+                }
+                
+                @Override
+                public void endElement(String uri, String localName, String qName) throws SAXException {
+                    if(localName.equals("appinfo")){
+                            parse = false;
+                    }
+                }
+
+                @Override
+                public void characters(char[] chars, int start, int length) throws SAXException {
+                    if(parse){
+                        StringBuilder sb = new StringBuilder();
+                        sb.append(chars, start, length);
+                        if(!sb.toString().replace(" ", "").trim().equals("")){
+                            String stringToAppend = addValue + " " + sb.toString().trim();
+                            stringBuilder.append(stringToAppend);
+                            stringBuilder.append("\n");
+                        }
+                    }
+                }
+
+                @Override
+                public void startPrefixMapping(String prefix, String uri) throws SAXException {}
+
+                @Override
+                public void endPrefixMapping(String prefix) throws SAXException {}
+
+                @Override
+                public void startDocument() throws SAXException {}
+                
+                @Override
+                public void endDocument() throws SAXException {}
+
+                @Override
+                public void skippedEntity(String name) throws SAXException {}
+
+                @Override
+                public void setDocumentLocator(Locator locator) {}
+
+                @Override
+                public void processingInstruction(String target, String data)throws SAXException {}
+
+                @Override
+                public void ignorableWhitespace(char[] chars, int start, int length)throws SAXException {}
+            };
 	}
 
 	@Override
-	public Object getResult(Object arg0) {
-		return stringBuilder.toString().trim();
+	public Object getResult(Object object) {
+            return stringBuilder.toString().trim();
 	}
-
 }
