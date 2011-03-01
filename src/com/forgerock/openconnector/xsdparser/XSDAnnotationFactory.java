0700b164eae8bb9665c3549e5d4630a19ecb8b30
olaschr <olaschr@05b3e5af-d696-470f-a577-fd7599f74d3c>
2011-03-01 14:29:13 +0000

    git-svn-id: https://svn.forgerock.org/openicf/branches/XML@6019 05b3e5af-d696-470f-a577-fd7599f74d3c

--- src/com/forgerock/openconnector/xsdparser/XSDAnnotationFactory.java
+++ src/com/forgerock/openconnector/xsdparser/XSDAnnotationFactory.java
@@ -1,3 +1,29 @@
+/*
+ *
+ * Copyright (c) 2010 ForgeRock Inc. All Rights Reserved
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://www.opensource.org/licenses/cddl1.php or
+ * OpenIDM/legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at OpenIDM/legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted 2010 [name of copyright owner]"
+ *
+ * $Id$
+ */
+
 package com.forgerock.openconnector.xsdparser;
 
 import com.sun.xml.xsom.parser.AnnotationParser;
@@ -9,5 +35,4 @@ public class XSDAnnotationFactory implements AnnotationParserFactory{
 	public AnnotationParser create() {
 		return new XSDAnnotationParser();
 	}
-
 }
