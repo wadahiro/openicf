688c8d1a11c569b0de2021ec05f49153f89178f5
jorgen.ringen <jorgen.ringen@05b3e5af-d696-470f-a577-fd7599f74d3c>
2011-03-01 15:25:05 +0000

    noenlunde fungerende versjon :----)
    git-svn-id: https://svn.forgerock.org/openicf/branches/XML@6027 05b3e5af-d696-470f-a577-fd7599f74d3c

--- src/com/forgerock/openconnector/xml/XMLFilterTranslator.java
+++ src/com/forgerock/openconnector/xml/XMLFilterTranslator.java
@@ -1,24 +1,27 @@
-/*
- * ====================
- * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
- * 
- * Copyright 2008-2009 Sun Microsystems, Inc. All rights reserved.     
- * 
- * The contents of this file are subject to the terms of the Common Development 
- * and Distribution License("CDDL") (the "License").  You may not use this file 
- * except in compliance with the License.
- * 
- * You can obtain a copy of the License at 
- * http://IdentityConnectors.dev.java.net/legal/license.txt
- * See the License for the specific language governing permissions and limitations 
- * under the License. 
- * 
- * When distributing the Covered Code, include this CDDL Header Notice in each file
- * and include the License file at identityconnectors/legal/license.txt.
- * If applicable, add the following below this CDDL Header, with the fields 
- * enclosed by brackets [] replaced by your own identifying information: 
- * "Portions Copyrighted [year] [name of copyright owner]"
- * ====================
+ /*
+  *
+ * Copyright (c) 2010 ForgeRock Inc. All Rights Reserved
+ *
+ * The contents of this file are subject to the terms
+ * of the Common Development and Distribution License
+ * (the License). You may not use this file except in
+ * compliance with the License.
+ *
+ * You can obtain a copy of the License at
+ * http://www.opensource.org/licenses/cddl1.php or
+ * OpenIDM/legal/CDDLv1.0.txt
+ * See the License for the specific language governing
+ * permission and limitations under the License.
+ *
+ * When distributing Covered Code, include this CDDL
+ * Header Notice in each file and include the License file
+ * at OpenIDM/legal/CDDLv1.0.txt.
+ * If applicable, add the following below the CDDL Header,
+ * with the fields enclosed by brackets [] replaced by
+ * your own identifying information:
+ * "Portions Copyrighted 2010 [name of copyright owner]"
+ *
+ * $Id$
  */
 package com.forgerock.openconnector.xml;
 
