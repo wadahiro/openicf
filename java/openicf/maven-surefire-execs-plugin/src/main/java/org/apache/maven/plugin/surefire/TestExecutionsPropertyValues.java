f54413231e87f5e0b0c98fd9fe100dd21c2d4b88
Laszlo <Laszlo@05b3e5af-d696-470f-a577-fd7599f74d3c>
2011-03-08 23:31:42 +0000

    fixing maven build after the previous update.
    git-svn-id: https://svn.forgerock.org/openicf/branches/openicf-proposal@6035 05b3e5af-d696-470f-a577-fd7599f74d3c

---
+++ java/openicf/maven-surefire-execs-plugin/src/main/java/org/apache/maven/plugin/surefire/TestExecutionsPropertyValues.java
@@ -0,0 +1,16 @@
+package org.apache.maven.plugin.surefire;
+
+
+public class TestExecutionsPropertyValues {
+	/**
+	 * @parameter
+	 * @required
+	 */
+	String propertyName;
+	/**
+	 * @parameter
+	 * @required
+	 */
+	String propertyValues;
+	
+}
